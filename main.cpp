a5d58cd0e94a8151622fbfe25fb2e29d82c45ba1
MathiasFar <mathiassvarre06@gmail.com>
2022-09-20 20:16:42 +0200

    gets all primes from 1 to a limit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+#define LIMIT 1000000
+
+using namespace std;
+
+bool* getPrimes() {
+    static bool isPrime[LIMIT];
+    fill(isPrime,  isPrime+LIMIT, true);
+
+    for(int i = 0; i < LIMIT; ++i) {
+        if(isPrime[i] && i != 0 && i != 1) {
+            int j = i*2;
+            while(j < LIMIT) {
+                isPrime[j] = false;
+                j += i;
+            }
+        }
+    }
+    return isPrime;
+}
+
+int main(){
+    bool *isPrime = getPrimes();
+
+    for(int i = 0; i < LIMIT; ++i) {
+        if(isPrime[i]) {
+            cout<<i<<endl;
+        }
+    }
+    
+    return 0;
+}
